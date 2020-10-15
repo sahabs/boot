bce942825cfd5f9bb1fa1e85978032e6d22b6add
B Saha <bee.saha@gmail.com>
2020-10-16 03:18:34 +0530

    first commit from ubuntu 20.04

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("First Spring Boot Project");
+		System.out.println("Tested from ubntu 20.04 on oct-16-20");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
